6269c666adfe050eb05487bb1a44dc988da19680
ANJANA K <anjanak2025473@gmail.com>
2026-05-23 16:56:18 +0530

    fix: add user setup to render.yaml buildCommand

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ python manage.py migrate
 
 python manage.py shell -c "
 from requests_app.models import User
+
 if not User.objects.filter(username='PROJECTADMIN').exists():
     User.objects.create_superuser('PROJECTADMIN', 'anjanak2025473@gmail.com', 'my_project2025')
     print('Superuser created!')
@@ -16,4 +17,14 @@ else:
     u.is_superuser = True
     u.save()
     print('Superuser password reset!')
+
+if not User.objects.filter(username='princi@123').exists():
+    User.objects.create_user(username='princi@123', password='Bmc@2026', email='principal@gmail.com', role='principal')
+    print('Principal created!')
+else:
+    p = User.objects.get(username='princi@123')
+    p.set_password('Bmc@2026')
+    p.role = 'principal'
+    p.save()
+    print('Principal password reset!')
 "
